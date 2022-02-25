eabb4188a6cd290610d26ede763328339e9d4835
tonyclem <tonyclem001@gmail.com>
2022-02-25 08:03:07 +0100

    bisary searcn and appendd-

--- binarySearch.js
+++ binarySearch.js
@@ -46,3 +46,23 @@ console.log(
     95
   )
 );
+
+// Refactored Version
+function binarySearch2(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+console.log(
+  binarySearch2(
+    [6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 95, 98, 99],
+    95
+  )
+);
