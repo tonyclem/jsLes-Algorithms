da25c06d26d842608eb00566de35f3b5a6bcdbb3
tonyclem <tonyclem001@gmail.com>
2022-02-28 09:19:19 +0100

    selection Sort and insertion Sort

--- selectionSort/selection.js
+++ selectionSort/selection.js
@@ -5,14 +5,13 @@
 // Repeat this with the next element until the array is sorted
 
 // LEGACY VERSION (non ES2015 syntax)
-function sselectionSort(arr) {
+function selectionSort1(arr) {
   for (var i = 0; i < arr.length; i++) {
     var lowest = i;
-    for (var j = i + 1; j < arr.length; j++) {
-      console.log(j);
+    for (var j = lowest + 1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) {
         lowest = j;
-        console.log("okay break na my guy: ", j);
+        console.log(lowest);
       }
     }
     if (i !== lowest) {
@@ -26,21 +25,20 @@ function sselectionSort(arr) {
 }
 
 // ES2015 VERSION
-// function selectionSort(arr) {
-//   const swap = (arr, idx1, idx2) =>
-//     ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
 
-//   for (let i = 0; i < arr.length; i++) {
-//     let lowest = i;
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[lowest] > arr[j]) {
-//         lowest = j;
-//       }
-//     }
-//     if (i !== lowest) swap(arr, i, lowest);
-//   }
+  return arr;
+}
 
-//   return arr;
-// }
-
-sselectionSort([0, 2, 34, 22, 10, 19, 17]);
+console.log(selectionSort1([0, 2, 34, 22, 10, 19, 17]));
