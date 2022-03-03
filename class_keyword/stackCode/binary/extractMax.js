4cad0a03e4331bc5104b63ec739a19106eb531de
tonyclem <tonyclem001@gmail.com>
2022-03-03 14:32:42 +0100

    added more lesson about Binary and Hash tables

--- class_keyword/stackCode/binary/extractMax.js
+++ class_keyword/stackCode/binary/extractMax.js
@@ -1,10 +1,14 @@
-// Insert
-// push the value into the values property on the heap
-// Bubble UP: Create a variable called index which is the length of the value property - 1
-// create a variable called parentIndex which is the floor of (index-1) / 2
-// keep looping as long as the value element at the parentIndex is less than value element at the child index
-// Swap the value of the values element at the parentIndex with the value of the element property at the child index
-// set the index to be parentIndex, and start over!
+// Remove
+// Swap the first value in the property with the last one
+// Pop from the value property, so you can return the value at the end.
+// Have the new root 'sink down' to the correct spot...
+// Your parent index starts at 0 (the root)
+// Find the index of the left child: 2 * index + 1 (make sure its not out of bounds)
+// Find the index of the right child: 2 * index + 2 (make sure its not out of bounds)
+// If the left or right child is greater than the element... swap. if both left and right children are larger, swap with largest child
+// The child index you swapped to noe becomes the new parent index.
+// keep looping and swapping until neither child is larger than the element
+// Return the old root!
 
 class MaxBinaryHeap {
   constructor() {
