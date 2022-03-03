c46b322ff80e6bc4273bce0a573d998a4064a788
tonyclem <tonyclem001@gmail.com>
2022-03-03 11:31:56 +0100

    move all binary files to binary folder

---
+++ class_keyword/stackCode/binary/deepFirstSearch3.js
@@ -0,0 +1,8 @@
+// DFS
+// create variable to store the value of nodes visited
+// Store the root of the BST in variable called current
+// write a helper function which accept a node
+// if the node has a left property, call the helper function with the left property on the node
+// push the value of the node to the variable that stores the values
+// if the node has a right property, call the helper function with the right property on the node
+// invoke the helper function with the current variable
